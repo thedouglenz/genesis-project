59f14b3ab3394a85055d2e7c1fcf42b0cc0853b5
Douglas Lenz <thedouglenz@gmail.com>
2026-02-17 11:13:54 -0500

    redirect to login on 401 response

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -12,4 +12,15 @@ client.interceptors.request.use((config) => {
   return config;
 });
 
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+      window.location.reload();
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default client;
